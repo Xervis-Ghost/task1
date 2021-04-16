2855f83637787c92278e1cd956a2e1873dd22772
Sabyasachi Chattopadhyay <82252986+Xervis-Ghost@users.noreply.github.com>
2021-04-16 10:54:54 +0530

    Create bysection.py

---
+++ byscetion.py
@@ -0,0 +1,27 @@
+import numpy as np 
+import matplotlib.pyplot as plt
+import scipy.optimize as fsolve
+
+def f(x):
+    return x**3 + 1
+def bisection(a,b,tol):
+    
+    xr=a
+    xl=b
+    while (np.abs(xr-xl)>=tol):
+        c=(xl+xr)/2
+        prod= f(c)*f(xl)
+        if prod >tol:
+            xl=c
+                
+        else:
+            if prod <tol:
+               xr=c
+    return c
+answer= bisection(-5,5,1e-8)
+print (answer)
+        
+x=np.linspace(-2,2,100)
+plt.plot(x,f(x))
+plt.grid()
+plt.show()
